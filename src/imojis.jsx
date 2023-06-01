5a840c8c990e9ef199f2edd22f2e1137b77f408f
Idrissa <idrisabkar2018@gmail.com>
2023-06-01 13:22:17 +0300

    init

---
+++ src/imojis.jsx
@@ -0,0 +1,115 @@
+export const emojis = [
+  {
+    name: 'Grinning Face',
+    meaning: 'A happy face with a big smile.',
+    emoji: '😀',
+  },
+  {
+    name: 'Thumbs Up',
+    meaning: 'An upward pointing thumb indicating approval or agreement.',
+    emoji: '👍',
+  },
+  {
+    name: 'Heart Eyes',
+    meaning: 'A face with heart-shaped eyes, expressing love or adoration.',
+    emoji: '😍',
+  },
+  {
+    name: 'Crying Face',
+    meaning:
+      'A face with a single tear, representing sadness or emotional pain.',
+    emoji: '😢',
+  },
+  {
+    name: 'Fire',
+    meaning: 'A flame representing heat, energy, or intense passion.',
+    emoji: '🔥',
+  },
+  {
+    name: 'Smiling Face with Sunglasses',
+    meaning:
+      'A cool face wearing sunglasses, indicating a carefree or confident attitude.',
+    emoji: '😎',
+  },
+  {
+    name: 'Rocket',
+    meaning: 'A rocket symbolizing speed, advancement, or success.',
+    emoji: '🚀',
+  },
+  {
+    name: 'Folded Hands',
+    meaning:
+      'Two hands placed together, expressing gratitude, prayer, or a wish for peace.',
+    emoji: '🙏',
+  },
+  {
+    name: 'Blue Heart',
+    meaning: 'A blue heart representing trust, harmony, and peace.',
+    emoji: '💙',
+  },
+  {
+    name: 'Smiling Face with Halo',
+    meaning:
+      'A smiling face with a halo above its head, often representing innocence or goodness.',
+    emoji: '😇',
+  },
+  {
+    name: 'Loudly Crying Face',
+    meaning:
+      'A face with streams of tears, expressing overwhelming sadness or grief.',
+    emoji: '😭',
+  },
+  {
+    name: 'Sunflower',
+    meaning:
+      'A yellow sunflower, symbolizing happiness, adoration, and vitality.',
+    emoji: '🌻',
+  },
+  {
+    name: 'Thumbs Down',
+    meaning:
+      'An downward pointing thumb indicating disapproval or disagreement.',
+    emoji: '👎',
+  },
+  {
+    name: 'Rainbow',
+    meaning:
+      'A colorful arc-shaped display in the sky, representing hope, diversity, or LGBTQ+ pride.',
+    emoji: '🌈',
+  },
+  {
+    name: 'Smiling Face with Heart-Eyes',
+    meaning:
+      'A face with heart-shaped eyes, expressing love, adoration, or extreme happiness.',
+    emoji: '😍',
+  },
+  {
+    name: 'Tropical Drink',
+    meaning:
+      'A beverage with a colorful umbrella, often associated with vacation, relaxation, or celebration.',
+    emoji: '🍹',
+  },
+  {
+    name: 'Party Popper',
+    meaning:
+      'A party popper exploding with confetti, representing celebration, excitement, or special occasions.',
+    emoji: '🎉',
+  },
+  {
+    name: 'Sleeping Face',
+    meaning:
+      'A face with closed eyes and a peaceful expression, indicating sleep or rest.',
+    emoji: '😴',
+  },
+  {
+    name: 'Rose',
+    meaning: 'A red rose, symbolizing love, romance, and beauty.',
+    emoji: '🌹',
+  },
+  {
+    name: 'Clapping Hands',
+    meaning:
+      'Two hands clapping together, often used to express applause, appreciation, or approval.',
+    emoji: '👏',
+  },
+]
